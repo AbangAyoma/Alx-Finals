6821856cace5b183daa9c187e0fec5da94351750
AbangAyoma <abangayoma@gmail.com>
2025-01-06 23:12:24 +0100

    created the login for handling user posts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    post = Post.object.all().order_by('-created_at')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'bolg/post_detail.html', {'post': post})
