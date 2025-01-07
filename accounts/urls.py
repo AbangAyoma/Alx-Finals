1fe7f5416b529b3e7f2ea13e7cdc3eef17400cda
AbangAyoma <abangayoma@gmail.com>
2025-01-07 23:10:36 +0100

    created register urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from .views import register
 
 urlpatterns = [
     # Login view
@@ -7,7 +8,9 @@ urlpatterns = [
     
     # Logout view
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-
+    
+    #login view abang
+      path('register/', register, name='register'),
     # Password reset views
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
