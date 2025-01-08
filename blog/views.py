72b8d663b0c8d05cf86945c3a0fab987904f47b4
AbangAyoma <abangayoma@gmail.com>
2025-01-08 10:51:05 +0100

    modified the post object create_at

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
-    post = Post.object.all().order_by('-created_at')
+    posts = Post.objects.all().order_by('-create_at')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
--- techsphere/settings.py
+++ techsphere/settings.py
@@ -36,7 +36,7 @@ LOGIN_URL = '/accounts/login/'
 # Application definition
 
 INSTALLED_APPS = [
-    'Accounts',
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
